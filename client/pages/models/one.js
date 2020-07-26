3bd79e2b07a7c1919920ef3383ace93334171533
bowei <bowei.han100@gmail.com>
2020-07-26 01:13:06 -0400

    v1 categories

--- client/pages/models/one.js
+++ client/pages/models/one.js
@@ -2,15 +2,18 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../../components/Layout";
 import utilStyles from "../../styles/utils.module.css";
 
-const name = "NLP Playground";
-
-export default function Home() {
+export default function ModelOne() {
   return (
     <Layout>
       <Head>
         <title>{siteTitle} - Model #1</title>
       </Head>
       <h1 className={utilStyles.heading2Xl}>Model #1</h1>
+      <h2 className={utilStyles.headingMd}>Boolean Response</h2>
+      <h2 className={utilStyles.headingMd}>Multiple Choice</h2>
+      <h2 className={utilStyles.headingMd}>Rating Scale</h2>
+      <h2 className={utilStyles.headingMd}>Name Identification</h2>
+      <h2 className={utilStyles.headingMd}>Numerical Identification</h2>
     </Layout>
   );
 }
