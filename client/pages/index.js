00c991300689e9dc1e3a77f55cc285d9fddd4e3a
bowei <bowei.han100@gmail.com>
2020-07-25 22:05:15 -0400

    basic home page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,44 +1,21 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/Layout";
 import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/posts";
 
-export default function Home({ allPostsData }) {
+const name = "NLP Playground";
+
+export default function Home() {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
+      <img
+        src="/images/bender.png"
+        className={`${utilStyles.headerHomeImage} ${utilStyles.borderCircle}`}
+        alt={name}
+      />
+      <h1 className={utilStyles.heading2Xl}>{name}</h1>
     </Layout>
   );
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
 }
