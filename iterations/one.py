5d0ce3f8da112250e25b16054b9d98ae6f472d4a
bowei <bowei.han100@gmail.com>
2020-08-09 23:14:21 -0400

    update bot error messages

--- iterations/one.py
+++ iterations/one.py
@@ -8,43 +8,45 @@ from bot.wordbank import no_words, yes_words
 class ExampleOne:
     def parseBoolean(self, input):
         if input.lower() in yes_words:
-            return "Great!"
+            return "Glad to hear that you have reliable transportation!"
         elif input.lower() in no_words:
-            return "That's too bad!"
+            return "Sorry to hear that you don't have reliable transportation."
         else:
-            return "I didn't understand your answer"
+            return "I didn't understand your answer."
 
     def parseMultiple(self, input):
         if input.lower() in set(["a", "toyota", "1"]):
-            return "A Toyota? Gotcha."
+            return "Toyota? Thanks."
         elif input.lower() in set(["b", "mazda", "2"]):
-            return "A Mazda? Gotcha."
+            return "Mazda? Thanks."
         elif input.lower() in set(["c", "lexus", "3"]):
-            return "A Lexus? Gotcha."
+            return "Lexus? Thanks."
         elif input.lower() in set(["d", "other", "4"]):
-            return "You've chosen Other. That's fine too."
+            return "Other? Thanks."
         else:
-            return input + " is not a valid option"
+            return input + " is not a valid option."
 
     def parseRating(self, input):
-        if input.lower() in set(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]):
-            return "Thanks, you've given a rating of: " + input
+        if input.lower() in set(["6", "7", "8", "9", "10"]):
+            return input + "? Glad to hear that you are doing well."
+        elif input.lower() in set(["1", "2", "3", "4", "5"]):
+            return input + "? I'm sorry to hear that."
         else:
-            return input + " is not a valid rating"
+            return input + " is not a valid rating."
 
     def parseName(self, input):
         nameList = StanzaRecognizer().recognize_persons(input)
 
         if not nameList:
-            return "Sorry, we were unable to identify your name."
+            return "Sorry, I was unable to identify your full name from your response."
 
-        return "Full Name: [" + " ".join(nameList) + "]"
+        return "Your full name is [" + " ".join(nameList) + "]."
 
     def parseNumber(self, input):
         numberList = StanzaRecognizer().recognize_numbers(input)
 
         if not numberList:
-            return "Sorry, we were unable to identify a number."
+            return "Sorry, I was unable to identify a valid salary in your response."
 
-        return "You've reported your salary as $" + numberList[0] + " / year"
+        return "You've reported your salary as $" + numberList[0] + " / year."
 
